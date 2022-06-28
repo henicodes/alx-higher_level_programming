262c321e25c03a6cb0521156bfce53407fc11a2b
henicodes <henicodes@gmail.com>
2022-06-28 19:40:01 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-asciiCode = 0
-while asciiCode < 26:
-    if asciiCode != 4 and asciiCode != 16:
-        print("{}".format(chr(97 + asciiCode)), end="")
-        asciiCode += 1
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end='')
