cc4a2ae6fde8425dcb0806847244ad08c2d71760
henicodes <henicodes@gmail.com>
2022-07-20 18:50:53 +0000

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """ class Square that defines a square"""
-    def __init__(self, size):
+        def __init__(self, size):
         """initialize square
         Args:
             size (int): size of the square
         """
-                self.__size = size  #: size of the square
+        self.__size = size  #: size of the square
